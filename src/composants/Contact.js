526ff04be5005d2a77487b4010111f8a2e837d1e
Rémi Cusset <56927808+lerem-s38@users.noreply.github.com>
2020-11-15 22:22:14 +0100

    navigation bar

---
+++ src/composants/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div className="card card-body mb-3">
+        <h4></h4>
+        
+        
+      </div>
+    )
+  }
+}
+
+export default Contact ; 
---
+++ src/composants/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-primary mb-3 py-O">
+      <div className="container">
+        <a href="/" className="navbar-brand">AppContacts</a>
+        
+
+          <ul className="navbar-nav">
+            <li className="nav-item m1-auto">
+              <a href="/" className="nav-link">Accueil</a>
+
+            </li>
+
+          </ul>
+      </div>
+      </nav>
+      
+    </div>
+  )
+}
+
+
+export default Header
+
+
